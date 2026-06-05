6e229c1e219c5c09686a76389fdfba964e0939d6
Richard Tingle <6330028+richardTingle@users.noreply.github.com>
2026-06-05 19:34:06 +0100

    Ensure images are ready for writing

--- jme3-screenshot-tests/jme3-screenshot-tests-shared/src/main/java/org/jmonkeyengine/screenshottests/testframework/TestReportCaptureBase.java
+++ jme3-screenshot-tests/jme3-screenshot-tests-shared/src/main/java/org/jmonkeyengine/screenshottests/testframework/TestReportCaptureBase.java
@@ -60,11 +60,14 @@ public abstract class TestReportCaptureBase {
     public abstract void attachImageInner(String title, String fileName, Image image);
 
     public static Image ensureRGBA8(Image image) {
+        Image correctImage;
         if(image.getFormat()!= Image.Format.RGBA8){
-            return convertToRGBA8(image);
+            correctImage = convertToRGBA8(image);
         } else{
-            return image;
+            correctImage = image;
         }
+        correctImage.getData(0).rewind();
+        return correctImage;
     }
 
     private static Image convertToRGBA8(Image rgbImage) {
